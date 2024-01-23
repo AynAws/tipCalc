161768c21ab339e1c324248aa51c71fe7e8c7864
GrimCreeperyt <grimcreeperyt@gmail.com>
2024-01-23 10:44:43 -0500

    a

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
 function calcAndDisplay() {
     let bill = parseFloat(document.getElementById('totalBill').value);
-    let numOfPpl = parseInt(document.getElementById('numOfPpl'));
+    let numOfPpl = parseInt(document.getElementById('numOfPpl').value);
     let service = document.getElementById('service').value;
-    let tip = calcTip(bill, service)
-    let totalBill = calcTotalBill(bill, tip)
-    let amntPerPrsn = calcAmntPerPrsn(totalBill, numOfPpl)
+    let tip = calcTip(bill, service);
+    let totalBill = calcTotalBill(bill, tip);
+    let amntPerPrsn = calcAmntPerPrsn(totalBill, numOfPpl);
 
     //Display Re
     document.getElementById('tipRe').innerText = 'Tip: $' + tip.toFixed(2);
-    document.getElementById('totalBillRe').innterText = 'Total Bill: $' + amntPerPrsn.toFixed(2);
-    //continued...
+    document.getElementById('totalBillRe').innterText = 'Total Bill: $' + totalBill.toFixed(2);
+    document.getElementById('amntPerPrsnRe').innerText = 'Amount Per Person: $' + amntPerPrsn.toFixed(2);
+}
+
+function calcTip(place1, place2) {
+    if (place2 === 'Great') {
+        return place1 * .2;
+    } else if (place2 === 'Good') {
+        return place1 * .15;
+    } else if (place2 === 'Poor') {
+        return place1 * .1;
+    } else {
+        alert('Invalid value. Please try again.');
+        return 0;
+    }
+}
+
+function calcTotalBill(place1, place2) {
+    place1 + place2;
+}
+
+function calcAmntPerPrsn(place1, place2) {
+    place1 / place2;
 }
 
 /* let bill = 120;
